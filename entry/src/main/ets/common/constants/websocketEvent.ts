04d0434e80f7251f78ab37a3cd79d4c4b2bdee54
gamejoye <gamejoye@gmail.com>
2024-04-03 21:09:35 +0800

    refactor(constant): websocket事件常量

--- entry/src/main/ets/common/constants/websocketEvent.ts
+++ entry/src/main/ets/common/constants/websocketEvent.ts
@@ -1,8 +1,8 @@
 export enum WebSocketEvent {
-  CHAT = 'chat',
-  NEW_MESSAGE = 'newMessage',
-  USER_ONLINE = 'userOnline',
-  USER_OFFLINE = 'userOffline',
-  USER_JOIN = 'userJoin',
-  USER_DEPARTURE = 'userDeparture',
+  CHAT = 1,
+  NEW_MESSAGE = 2,
+  USER_ONLINE = 3,
+  USER_OFFLINE = 4,
+  USER_JOIN = 5,
+  USER_DEPARTURE = 6,
 }
