5eed63fb59f2ae11928cf036bae38abcc78b0d9c
gamejoye <gamejoye@gmail.com>
2024-05-06 17:55:25 +0800

    perf(types): IChatChannel new prop joinTime

--- entry/src/main/ets/common/types/ChatChannel.type.ts
+++ entry/src/main/ets/common/types/ChatChannel.type.ts
@@ -4,6 +4,7 @@ import { IMessage } from './Message.type';
 export interface IChatChannel {
   latestMessage: IMessage | null;
   latestVisitTime: string;
+  joinTime: string;
   unreadMessageCount: number;
   onlineUserIds: number[];
   chatroom: IChatroom;
