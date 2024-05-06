15f9f4b4cb498ab0fd81855832bc1d5bf201b92f
gamejoye <gamejoye@gmail.com>
2024-05-06 17:46:26 +0800

    perf(utils): 用例边界处理

--- entry/src/main/ets/common/utils/http.ts
+++ entry/src/main/ets/common/utils/http.ts
@@ -1,4 +1,5 @@
 import http from '@ohos.net.http';
 export function isSuccess(statusCode: http.ResponseCode): boolean {
+  if ((statusCode + '').length !== 3) return false;
   return (statusCode + '').startsWith('2');
 }
